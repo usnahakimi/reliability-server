5ef19db0e01dce0bb816a6e879ae05cb6b2add12
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-09-15 12:24:45 +0100

    app.js update with post

--- app.js
+++ app.js
@@ -46,6 +46,53 @@ app.get('/*', async (req, res) => {
   res.status(upstreamResponse.status).send(await upstreamResponse.text())
 })
 
+app.get('/*', async (req, res) => {
+  let requestPath = req.originalUrl // => e.g. /hospitals
+  console.log(`:: GET ${requestPath}`) // => :: GET ${/hospitals}
+
+  let attemptsLeft = 3;
+  let upstreamResponse; // -> response from HOSP Server
+
+  while (attemptsLeft > 0) {
+    let upstream = `http://${TARGET_SERVER}${requestPath}`;
+		// e.g. http://ec2-url/hospitals
+
+    console.log(`:: Attempt ${3 - attemptsLeft}: ${upstream}`)
+
+    attemptsLeft = attemptsLeft - 1
+
+    upstreamResponse = await fetch(upstream, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        // 'Content-Type': 'application/json',
+    
+        'Authorization': req.header('Authorization')                          
+    }
+    })
+// Perform GET request against http://ec2-url/hospitals, with authentication credentials needed (as per the HOSP API documentation)
+
+    if (upstreamResponse.ok) {
+      let text = await upstreamResponse.text()
+      res.header('Content-Type', upstreamResponse.headers.get('content-type'))
+         .status(upstreamResponse.status) // -> 200
+         .send(text) /*
+{
+  "id": 1,
+  "name": "Jimbo Hospital",
+  "created_at": "2021-03-06T15:46:28.245Z",
+  "updated_at": "2021-03-06T15:46:28.245Z",
+  "url": "http://localhost:3000/hospitals/1.json"
+}
+					*/
+      console.log(":: Successful!")
+      return
+    }
+  }
+  console.log(`:: Failed GET ${requestPath}`)
+  res.status(upstreamResponse.status).send(await upstreamResponse.text())
+})
+
 app.listen('80', function () {
   console.log('Listening on port 80!');
 });
